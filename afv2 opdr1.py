81f16175eb568a6039bc4c6a2bd38fb7f669321b
Morgan Atmodimedjo <43204709+morganatmo@users.noreply.github.com>
2018-10-28 20:02:49 +0100

    Add files via upload

---
+++ afv2 opdr1.py
@@ -0,0 +1,15 @@
+#AFVINKOPDRACHT 2 - OPDRACHT 1
+#Morgan Atmodimedjo BIN1a
+
+naam = input("voer uw naam in: ")
+adres = input("voer uw stad, land en postcode in: ")
+telefoonnummer = int (input("voer uw telefoonnummer in: "))
+studie = input("voer uw studie in: ")
+#dit is de luie manier
+informatie = print("uw gegevens: ", naam, adres, telefoonnummer, studie)
+#dit is de overzichtelijke manier
+print("uw gegevens: ")
+print( naam)
+print( adres)
+print( telefoonnummer)
+print( studie)
